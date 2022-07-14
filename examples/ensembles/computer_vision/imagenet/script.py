fb422c9b98063febdb662dc1b499735a4c550475
Garrett Goon <garrett.goon@hpe.com>
2022-07-14 16:08:05 -0400

    script improvements

--- examples/ensembles/computer_vision/imagenet/script.py
+++ examples/ensembles/computer_vision/imagenet/script.py
@@ -16,7 +16,7 @@ parser.add_argument("-mc", "--model_criteria", type=str, default="small")
 parser.add_argument("-n", "--name", type=str, default="")
 parser.add_argument("-w", "--workspace", type=str, default="Ensembling")
 parser.add_argument("-cpp", "--checkpoint_path_prefix", type=str, default="shared_fs/state_dicts/")
-parser.add_argument("-nb", "--num_base_models", type=int, default=1)
+parser.add_argument("-nb", "--num_base_models", nargs="+", type=int)
 parser.add_argument("-ne", "--num_ensembles", type=int, default=1)
 parser.add_argument("-o", "--offset", type=int, default=0)
 parser.add_argument("-tb", "--train_batch_size", type=int, default=128)
@@ -28,61 +28,53 @@ args = parser.parse_args()
 
 client.login(master=args.master, user=args.user, password=args.password)
 
-# Generate name from command line arguments, if none provided.
-if not args.name:
-    args.name = f"{args.model_criteria}_{args.ensemble_strategy}_{args.num_base_models}"
+for num_base_models in args.num_base_models:
+    # Generate name from command line arguments, if none provided.
+    if not args.name:
+        args.name = f"{args.model_criteria}_{args.ensemble_strategy}_{num_base_models}"
 
-config = {
-    "entrypoint": "python -m determined.launch.torch_distributed -- python -m main",
-    "name": args.name,
-    "workspace": args.workspace + ("_test" if args.test else ""),
-    "project": args.dataset_name,
-    "max_restarts": 0,
-    "reproducibility": {"experiment_seed": 42},
-    "resources": {"slots_per_trial": 1},
-    "searcher": {"name": "single", "max_length": 1, "metric": "val_loss"},
-    "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
-    "hyperparameters": {
-        "train_batch_size": args.train_batch_size,
-        "val_batch_size": args.val_batch_size,
-        "dataset_name": args.dataset_name,
-        "skip_train": args.skip_train,
-        "ensemble_strategy": args.ensemble_strategy,
-        "model_criteria": args.model_criteria,
-        "sanity_check": args.sanity_check,
-        "num_base_models": args.num_base_models,
-        "checkpoint_path_prefix": args.checkpoint_path_prefix,
-    },
-}
+    config = {
+        "entrypoint": "python -m determined.launch.torch_distributed -- python -m main",
+        "name": args.name,
+        "workspace": args.workspace + ("_test" if args.test else ""),
+        "project": args.dataset_name,
+        "max_restarts": 0,
+        "reproducibility": {"experiment_seed": 42},
+        "resources": {"slots_per_trial": 1},
+        "searcher": {"name": "single", "max_length": 1, "metric": "val_loss"},
+        "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
+        "hyperparameters": {
+            "train_batch_size": args.train_batch_size,
+            "val_batch_size": args.val_batch_size,
+            "dataset_name": args.dataset_name,
+            "skip_train": args.skip_train,
+            "ensemble_strategy": args.ensemble_strategy,
+            "model_criteria": args.model_criteria,
+            "sanity_check": args.sanity_check,
+            "num_base_models": num_base_models,
+            "checkpoint_path_prefix": args.checkpoint_path_prefix,
+        },
+    }
 
+    workspace = workspaces.Workspace(
+        workspace_name=config["workspace"],
+        master_url=args.master,
+        username=args.user,
+        password=args.password,
+    )
+    workspace.create_project(config["project"])
 
-workspace = workspaces.Workspace(
-    workspace_name=config["workspace"],
-    master_url=args.master,
-    username=args.user,
-    password=args.password,
-)
-workspace.create_project(config["project"])
-
-ensembles = timm_models.get_timm_ensembles_of_model_names(
-    model_criteria=args.model_criteria,
-    num_base_models=args.num_base_models,
-    num_ensembles=args.num_ensembles,
-    offset=args.offset,
-)
-
-for model_names in ensembles:
-    config["hyperparameters"]["model_names"] = model_names
-    client.create_experiment(config=config, model_dir=".")
-    print(
-        f"{args.num_base_models} models in ensemble: {config['hyperparameters']['model_names']}",
-        "\n",
+    ensembles = timm_models.get_timm_ensembles_of_model_names(
+        model_criteria=args.model_criteria,
+        num_base_models=num_base_models,
+        num_ensembles=args.num_ensembles,
+        offset=args.offset,
     )
 
-print(
-    "",
-    80 * "*",
-    f"Submitted {len(ensembles)} experiments to master at {args.master}",
-    80 * "*",
-    sep="\n",
-)
+    for model_names in ensembles:
+        config["hyperparameters"]["model_names"] = model_names
+        client.create_experiment(config=config, model_dir=".")
+        print(
+            f"{num_base_models} models in ensemble: {config['hyperparameters']['model_names']}",
+            "\n",
+        )
