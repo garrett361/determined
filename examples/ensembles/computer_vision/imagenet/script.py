f8367fc963becd0b1df62987d68d19d8017ac58a
Garrett Goon <garrett.goon@hpe.com>
2022-07-14 14:01:48 -0400

    bug fixes

--- examples/ensembles/computer_vision/imagenet/script.py
+++ examples/ensembles/computer_vision/imagenet/script.py
@@ -18,46 +18,47 @@ parser.add_argument("-vb", "--val_batch_size", type=int, default=256)
 parser.add_argument("-d", "--dataset_name", type=str, default="imagenette2-160")
 parser.add_argument("-st", "--skip_train", type=bool, default=True)
 parser.add_argument("-es", "--ensemble_strategy", type=str, default="naive")
-parser.add_argument("-mc", "--model_criteria", type=str, default="smallest")
+parser.add_argument("-mc", "--model_criteria", type=str, default="small")
 parser.add_argument("-n", "--name", type=str, default="test")
 parser.add_argument("-w", "--workspace", type=str, default="Ensembling")
 parser.add_argument("-sc", "--sanity_check", type=bool, default=True)
 parser.add_argument("-t", "--test", type=bool, default=True)
+parser.add_argument("-cpp", "--checkpoint_path_prefix", type=str, default="shared_fs/state_dicts/")
 args = parser.parse_args()
 
 client.login(master=args.master, user=args.user, password=args.password)
 
-config = attrdict.AttrDict(
-    {
-        "entrypoint": "python -m determined.launch.torch_distributed -- python -m main",
-        "name": args.name,
-        "workspace": args.workspace + ("_test" if args.test else ""),
-        "project": args.dataset_name,
-        "max_restarts": 0,
-        "reproducibility": {"experiment_seed": 42},
-        "resources": {"slots_per_trial": 1},
-        "searcher": {"name": "single", "max_length": 1, "metric": "val_loss"},
-        "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
-        "hyperparameters": {
-            "train_batch_size": args.train_batch_size,
-            "val_batch_size": args.val_batch_size,
-            "dataset_name": args.dataset_name,
-            "skip_train": args.skip_train,
-            "ensemble_strategy": args.ensemble_strategy,
-            "model_criteria": args.model_criteria,
-            "sanity_check": args.sanity_check,
-            "num_base_models": args.num_base_models,
-        },
-    }
-)
+config = {
+    "entrypoint": "python -m determined.launch.torch_distributed -- python -m main",
+    "name": args.name,
+    "workspace": args.workspace + ("_test" if args.test else ""),
+    "project": args.dataset_name,
+    "max_restarts": 0,
+    "reproducibility": {"experiment_seed": 42},
+    "resources": {"slots_per_trial": 1},
+    "searcher": {"name": "single", "max_length": 1, "metric": "val_loss"},
+    "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
+    "hyperparameters": {
+        "train_batch_size": args.train_batch_size,
+        "val_batch_size": args.val_batch_size,
+        "dataset_name": args.dataset_name,
+        "skip_train": args.skip_train,
+        "ensemble_strategy": args.ensemble_strategy,
+        "model_criteria": args.model_criteria,
+        "sanity_check": args.sanity_check,
+        "num_base_models": args.num_base_models,
+        "checkpoint_path_prefix": args.checkpoint_path_prefix,
+    },
+}
+
 
 workspace = workspaces.Workspace(
-    workspace_name=config.workspace,
+    workspace_name=config["workspace"],
     master_url=args.master,
     username=args.user,
     password=args.password,
 )
-workspace.create_project(config.project)
+workspace.create_project(config["project"])
 
 ensembles = timm_models.get_timm_ensembles_of_model_names(
     model_criteria=args.model_criteria,
@@ -67,8 +68,9 @@ ensembles = timm_models.get_timm_ensembles_of_model_names(
 )
 
 for model_names in ensembles:
+    config["hyperparameters"]["model_names"] = model_names
     client.create_experiment(config=config, model_dir=".")
-    print(f"Models in ensemble: {config.hyperparameters.model_names}")
+    print(f"Models in ensemble: {config['hyperparameters']['model_names']}")
 
 print(
     "",
--- examples/ensembles/computer_vision/imagenet/timm_models.py
+++ examples/ensembles/computer_vision/imagenet/timm_models.py
@@ -5,44 +5,24 @@ least 75% accuracy on the same task.
 
 import itertools
 import math
-from typing import Literal, List, Union
 import random
+from typing import Literal, List, Union
+import pickle
 
 import timm
 import torch.nn as nn
 
 
-timm_best_models = {
-    "beit_large_patch16_512": "",
-    "beit_large_patch16_384": "",
-    "tf_efficientnet_l2_ns": "",
-    "tf_efficientnet_l2_ns_475": "",
-    "convnext_xlarge_384_in22ft1k": "",
-    "beit_large_patch16_224": "",
-    "convnext_large_384_in22ft1k": "",
-    "swin_large_patch4_window12_384": "",
-    "vit_large_patch16_384": "",
-    "volo_d5_512": "",
-}
+with open("small_timm_models.pkl", "rb") as f:
+    small_timm_models = pickle.load(f)
+with open("top_timm_models.pkl", "rb") as f:
+    top_timm_models = pickle.load(f)
 
-timm_smallest_models_75_top1_masked = {
-    "xcit_nano_12_p16_384_dist": "xcit_nano_12_p16_384_dist.pth",
-    "xcit_nano_12_p8_384_dist": "xcit_nano_12_p8_384_dist.pth",
-    "xcit_nano_12_p8_224_dist": "xcit_nano_12_p8_224_dist.pth",
-    "semnasnet_100": "mnasnet_a1-d9418771.pth",
-    "mixnet_s": "mixnet_s-a907afbc.pth",
-    "tf_mixnet_s": "tf_mixnet_s-89d3354b.pth",
-    "mobilenetv2_110d": "mobilenetv2_110d_ra-77090ade.pth",
-    "efficientnet_lite0": "efficientnet_lite0_ra-37913777.pth",
-    "rexnet_100": "rexnetv1_100-1b4dddf4.pth",
-    "mixnet_m": "mixnet_m-4647fc68.pth",
-}
-
-all_models = {**timm_best_models, **timm_smallest_models_75_top1_masked}
+all_models = {**small_timm_models, **top_timm_models}
 
 
 def get_timm_ensembles_of_model_names(
-    model_criteria: Literal["best", "smallest"],
+    model_criteria: Literal["top", "small"],
     num_base_models: int,
     num_ensembles: int,
     seed: int = 42,
@@ -51,10 +31,10 @@ def get_timm_ensembles_of_model_names(
     """Returns num_ensembles unique ensembles of timm model names, each comprising of
     num_base_models models.  Use num_ensembles = -1 to get all possible ensembles.
     """
-    if model_criteria == "best":
-        base_models = timm_best_models
-    elif model_criteria == "smallest":
-        base_models = timm_smallest_models_75_top1_masked
+    if model_criteria == "top":
+        base_models = top_timm_models
+    elif model_criteria == "small":
+        base_models = small_timm_models
     else:
         raise ValueError(f"Unknown model_criteria: {model_criteria}")
     assert (
@@ -80,9 +60,7 @@ def get_timm_ensembles_of_model_names(
     return ensembles[offset:]
 
 
-def build_timm_model_list(
-    model_names: List[str], checkpoint_path_prefix: str = "shared_fs/state_dicts/checkpoints/"
-) -> List[nn.Module]:
+def build_timm_model_list(model_names: List[str], checkpoint_path_prefix: str) -> List[nn.Module]:
     """Returns a list of models, each of which is a timm model."""
     models = []
     for model_name in model_names:
