8cdca099382483e05a16fcc702e89a1d808768c8
Garrett Goon <garrett.goon@hpe.com>
2022-07-19 14:27:23 -0400

    added multiple strategies in script

--- examples/ensembles/computer_vision/imagenet/script.py
+++ examples/ensembles/computer_vision/imagenet/script.py
@@ -28,7 +28,7 @@ parser.add_argument("-m", "--master", type=str, default="localhost:8080")
 parser.add_argument("-u", "--user", type=str, default="determined")
 parser.add_argument("-p", "--password", type=str, default="")
 parser.add_argument("-d", "--dataset_name", type=str, default="imagenette2-160")
-parser.add_argument("-es", "--ensemble_strategy", type=str, default="")
+parser.add_argument("-es", "--ensemble_strategy", nargs="+", type=str, default=[])
 parser.add_argument("-mc", "--model_criteria", type=str, default="")
 parser.add_argument("-n", "--name", type=str, default="")
 parser.add_argument("-w", "--workspace", type=str, default="Ensembling")
@@ -66,10 +66,12 @@ workspace_name = args.workspace + ("_test" if args.test else "")
 client.login(master=args.master, user=args.user, password=args.password)
 
 if args.num_ensembles != -1:
-    num_experiments = len(args.num_base_models) * args.num_ensembles
+    num_experiments = len(args.ensemble_strategy) * len(args.num_base_models) * args.num_ensembles
 else:
     base_model_collection_size = len(timm_models.get_model_names_from_criteria(args.model_criteria))
-    num_experiments = sum(math.comb(base_model_collection_size, n) for n in args.num_base_models)
+    num_experiments = len(args.ensemble_strategy) * sum(
+        math.comb(base_model_collection_size, n) for n in args.num_base_models
+    )
 
 # Safety valve for accidentally running a lot of experiments.
 if num_experiments >= 100:
@@ -77,67 +79,69 @@ if num_experiments >= 100:
     if confirm != "yes":
         sys.exit("Cancelling experiment creation.")
 
-s_or_blank = "s" if num_experiments != 1 else ""
-print(
-    80 * "-",
-    f"\nSubmitting {num_experiments} {args.ensemble_strategy} experiment{s_or_blank}",
-    f"to workspace {workspace_name}\n",
-    80 * "-",
-    "\n",
-)
-
-for num_base_models in args.num_base_models:
-    if generate_names:
-        name_components = [
-            f"{'manual' if args.model_names else args.model_criteria}",
-            f"{args.ensemble_strategy}",
-            f"{num_base_models}",
-        ]
-        args.name = "_".join(name_components)
-
-    config = {
-        "entrypoint": "python -m determined.launch.torch_distributed -- python -m main",
-        "name": args.name,
-        "workspace": workspace_name,
-        "project": args.dataset_name,
-        "max_restarts": 0,
-        "reproducibility": {"experiment_seed": 42},
-        "resources": {"slots_per_trial": 1},
-        "searcher": {"name": "single", "max_length": 1, "metric": "val_top1_acc"},
-        "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
-        "hyperparameters": {
-            "train_batch_size": args.train_batch_size,
-            "val_batch_size": args.val_batch_size,
-            "dataset_name": args.dataset_name,
-            "ensemble_strategy": args.ensemble_strategy,
-            "model_criteria": args.model_criteria,
-            "sanity_check": args.sanity_check,
-            "num_combinations": args.num_combinations,
-            "num_base_models": num_base_models,
-            "checkpoint_path_prefix": args.checkpoint_path_prefix,
-            "lr": args.learning_rate,
-        },
-    }
-
-    workspace = workspaces.Workspace(
-        workspace_name=config["workspace"],
-        master_url=args.master,
-        username=args.user,
-        password=args.password,
+for strategy in args.ensemble_strategy:
+    s_or_blank = "s" if num_experiments != 1 else ""
+    print(
+        80 * "-",
+        f"\nSubmitting {num_experiments} {strategy} experiment{s_or_blank}",
+        f"to workspace {workspace_name}\n",
+        80 * "-",
+        "\n",
     )
-    workspace.create_project(config["project"])
 
-    if args.model_names:
-        ensembles = [args.model_names]
-    else:
-        ensembles = timm_models.get_timm_ensembles_of_model_names(
-            model_criteria=args.model_criteria,
-            num_base_models=num_base_models,
-            num_ensembles=args.num_ensembles,
-            offset=args.offset,
+for strategy in args.ensemble_strategy:
+    for num_base_models in args.num_base_models:
+        if generate_names:
+            name_components = [
+                f"{'manual' if args.model_names else args.model_criteria}",
+                f"{strategy}",
+                f"{num_base_models}",
+            ]
+            args.name = "_".join(name_components)
+
+        config = {
+            "entrypoint": "python -m determined.launch.torch_distributed -- python -m main",
+            "name": args.name,
+            "workspace": workspace_name,
+            "project": args.dataset_name,
+            "max_restarts": 0,
+            "reproducibility": {"experiment_seed": 42},
+            "resources": {"slots_per_trial": 1},
+            "searcher": {"name": "single", "max_length": 1, "metric": "val_top1_acc"},
+            "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
+            "hyperparameters": {
+                "train_batch_size": args.train_batch_size,
+                "val_batch_size": args.val_batch_size,
+                "dataset_name": args.dataset_name,
+                "ensemble_strategy": strategy,
+                "model_criteria": args.model_criteria,
+                "sanity_check": args.sanity_check,
+                "num_combinations": args.num_combinations,
+                "num_base_models": num_base_models,
+                "checkpoint_path_prefix": args.checkpoint_path_prefix,
+                "lr": args.learning_rate,
+            },
+        }
+
+        workspace = workspaces.Workspace(
+            workspace_name=config["workspace"],
+            master_url=args.master,
+            username=args.user,
+            password=args.password,
         )
-    desc = f"{num_base_models} model{'s' if num_base_models != 1 else ''} ensembles"
-    for model_names in tqdm.tqdm(ensembles, desc=desc):
-        config["hyperparameters"]["model_names"] = model_names
-        with suppress_stdout():
-            client.create_experiment(config=config, model_dir=".")
+        workspace.create_project(config["project"])
+
+        if args.model_names:
+            ensembles = [args.model_names]
+        else:
+            ensembles = timm_models.get_timm_ensembles_of_model_names(
+                model_criteria=args.model_criteria,
+                num_base_models=num_base_models,
+                num_ensembles=args.num_ensembles,
+                offset=args.offset,
+            )
+        desc = f"{num_base_models} model{'s' if num_base_models != 1 else ''} ensembles"
+        for model_names in tqdm.tqdm(ensembles, desc=desc):
+            config["hyperparameters"]["model_names"] = model_names
+            with suppress_stdout():
+                client.create_experiment(config=config, model_dir=".")
