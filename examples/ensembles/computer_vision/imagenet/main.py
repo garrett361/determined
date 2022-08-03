909edf9d397db73a8311bc117a1107e71b8d075e
Garrett Goon <garrett.goon@hpe.com>
2022-08-03 14:17:55 -0400

    use timm resolve_data_config for transform kwargs

--- examples/ensembles/computer_vision/imagenet/main.py
+++ examples/ensembles/computer_vision/imagenet/main.py
@@ -13,9 +13,7 @@ def main(core_context, hparams: Dict[str, Any]) -> None:
     models = timm_models.build_timm_models(
         model_names=hparams.model_names, checkpoint_path_prefix=hparams.checkpoint_path_prefix
     )
-    transforms = data.build_timm_transforms(
-        model_names=hparams.model_names, dataset_name=hparams.dataset_name
-    )
+    transforms = data.build_timm_transforms(models=models)
     trainer = ensembles.Ensemble(
         core_context,
         models=models,
