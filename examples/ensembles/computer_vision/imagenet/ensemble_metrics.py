df798259272b74ab2754f91cd0997c80ffb0de07
Garrett Goon <garrett.goon@hpe.com>
2022-08-03 21:43:50 -0400

    minimal implementation

--- examples/ensembles/computer_vision/imagenet/ensemble_metrics.py
+++ examples/ensembles/computer_vision/imagenet/ensemble_metrics.py
@@ -1,14 +1,14 @@
-from typing import Any, Union
+from typing import Union
 
 import torch
-from torch import Tensor
 
 from torchmetrics.aggregation import BaseAggregator
 
 
-class ZeroDimMeanMetric(BaseAggregator):
-    """Computes simple, vectorized means, with the mean taken only over
-    the zero dimension.
+class BatchDimMeanMetric(BaseAggregator):
+    """Computes vectorized means over the batch dimension, assumed to be the leading dimension.
+    E.g., the compute method returns the sum of all passed in tensor values divided by the total
+    number of processed records.
     """
 
     def __init__(
@@ -22,16 +22,8 @@ class ZeroDimMeanMetric(BaseAggregator):
         )
         self.add_state("records", default=torch.tensor(0.0), dist_reduce_fx="sum")
 
-    def update(self, value: Union[float, Tensor]) -> None:  # type: ignore
-        """Update state with data.
-        Args:
-            value: Either a float or tensor containing data. Additional tensor
-                dimensions will be flattened
-            weight: Either a float or tensor containing weights for calculating
-                the average. Shape of weight should be able to broadcast with
-                the shape of `value`. Default to `1.0` corresponding to simple
-                harmonic average.
-        """
+    def update(self, value: torch.Tensor) -> None:  # type: ignore
+        """Update state with data."""
         value = self._cast_and_nan_check_input(value)
 
         if value.numel() == 0:
@@ -39,9 +31,37 @@ class ZeroDimMeanMetric(BaseAggregator):
         self.value = self.value + value.sum(0)
         self.records += value.shape[0]
 
-    def compute(self) -> Tensor:
+    def compute(self) -> torch.Tensor:
         """Compute the aggregated value."""
         return self.value / self.records
+
+
+class VectorizedMeanMetric(BaseAggregator):
+    """Computes vectorized means, averaged over the number of consumed tensors."""
+
+    def __init__(
+        self,
+        nan_strategy: Union[str, float] = "warn",
+    ):
+        super().__init__(
+            "sum",
+            torch.tensor(0.0),
+            nan_strategy,
+        )
+        self.add_state("updates", default=torch.tensor(0.0), dist_reduce_fx="sum")
+
+    def update(self, value: torch.Tensor) -> None:  # type: ignore
+        """Update state with data."""
+        value = self._cast_and_nan_check_input(value)
+
+        if value.numel() == 0:
+            return
+        self.value = self.value + value
+        self.updates += 1
+
+    def compute(self) -> torch.Tensor:
+        """Compute the aggregated value."""
+        return self.value / self.updates
 
 
 class MultiModelAccuracy(BaseAggregator):
@@ -64,16 +84,8 @@ class MultiModelAccuracy(BaseAggregator):
         self.k = k
         self.add_state("records", default=torch.tensor(0.0), dist_reduce_fx="sum")
 
-    def update(self, output: Tensor, labels: Tensor) -> None:  # type: ignore
-        """Update state with data.
-        Args:
-            value: Either a float or tensor containing data. Additional tensor
-                dimensions will be flattened
-            weight: Either a float or tensor containing weights for calculating
-                the average. Shape of weight should be able to broadcast with
-                the shape of `value`. Default to `1.0` corresponding to simple
-                harmonic average.
-        """
+    def update(self, output: torch.Tensor, labels: torch.Tensor) -> None:  # type: ignore
+        """Update state with data."""
         output = self._cast_and_nan_check_input(output)
         labels = self._cast_and_nan_check_input(labels)
         records = output.shape[0]
@@ -81,7 +93,7 @@ class MultiModelAccuracy(BaseAggregator):
             return
         assert (
             len(output.shape) == 3
-        ), "output should be a 3D tensor, consisting of batch, pred, and model dimss, in that order"
+        ), "output should be a 3D tensor, consisting of batch, pred, and model dims, in that order"
 
         preds = output.topk(k=self.k, dim=1).indices
         non_model_dims = len(preds.shape) - 1
@@ -93,6 +105,6 @@ class MultiModelAccuracy(BaseAggregator):
         self.value = self.value + correct_preds
         self.records += records
 
-    def compute(self) -> Tensor:
+    def compute(self) -> torch.Tensor:
         """Compute the aggregated value."""
         return self.value / self.records
