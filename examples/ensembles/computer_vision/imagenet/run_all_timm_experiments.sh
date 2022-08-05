fa8a94a97766559cf069463e7b7045d9e70a2aa6
Garrett Goon <garrett.goon@hpe.com>
2022-08-04 20:49:19 -0400

    minor changes

--- examples/ensembles/computer_vision/imagenet/run_all_timm_experiments.sh
+++ examples/ensembles/computer_vision/imagenet/run_all_timm_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Master URL: " MASTER_URL
 read -p "Number of base models to use (Default 2 3 4 5): " NUM_BASE_MODELS
-read -p "Number of Ensembles per strategy, when using > 1 base models (Default 100): " NUM_ENSEMBLES
+read -p "Number of Ensembles per strategy (Default 100): " NUM_ENSEMBLES
 read -p "Number of combinations for VBMC (Default 512): " NUM_COMBINATIONS
 read -p "Number of training epochs, when applicable (Default 1): " EPOCHS
 read -p "Learning rate for SGD training (Default .001): " LR
@@ -12,7 +12,7 @@ echo -e "\nThis will run all ensembling experiments with the following parameter
 
 echo "Master URL: $MASTER_URL"
 echo "Number of base models: ${NUM_BASE_MODELS:=2 3 4 5}"
-echo "Number of Ensembles per strategy, when using > 1 base models: ${NUM_ENSEMBLES:=100}"
+echo "Number of Ensembles per strategy: ${NUM_ENSEMBLES:=100}"
 echo "Number of combinations for VBMC: ${NUM_COMBINATIONS:=512}"
 echo "Number of training epochs for SGD training: ${EPOCHS:=1}"
 echo "Learning rate for SGD training: ${LR:=.001}"
@@ -30,5 +30,5 @@ esac
 
 # Run $NUM_ENSEMBLES ensembles for every X-model ensembles, X in "$NUM_BASE_MODELS"
 python3 script_timm.py -m "$MASTER_URL" -nbm "$NUM_BASE_MODELS" -ne "$NUM_ENSEMBLES" -es naive naive_logits most_confident majority_vote -mc "$MODEL_CRITERIA" --no_safety_check --delete_unvalidated
-python3 script_timm.py -m "$MASTER_URL" -nbm "$NUM_BASE_MODELS" -ne "$NUM_ENSEMBLES" -es naive_temp naive_logits_temp most_confident_temp super_learner_probs super_learner_logits -mc "$MODEL_CRITERIA" --no_safety_check -e "$EPOCHS"
+python3 script_timm.py -m "$MASTER_URL" -nbm "$NUM_BASE_MODELS" -ne "$NUM_ENSEMBLES" -es naive_temp naive_logits_temp most_confident_temp super_learner_probs super_learner_probs_temp super_learner_logits -mc "$MODEL_CRITERIA" --no_safety_check -e "$EPOCHS"
 python3 script_timm.py -m "$MASTER_URL" -nbm "$NUM_BASE_MODELS" -ne "$NUM_ENSEMBLES" -es vbmc vbmc_temp -mc "$MODEL_CRITERIA" -nc "$NUM_COMBINATIONS" --no_safety_check
