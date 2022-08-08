59d1fbf15a41818a0aef0b08f76fb0be065b30e8
Garrett Goon <garrett.goon@hpe.com>
2022-08-08 18:17:08 -0400

    refactoring and starting checkpoints

--- examples/computer_vision/mnist_pytorch_core_api/mnist_pytorch_core_api.py
+++ examples/computer_vision/mnist_pytorch_core_api/mnist_pytorch_core_api.py
@@ -6,6 +6,7 @@ import torch.distributed as dist
 import torch.nn as nn
 import torchvision.datasets as datasets
 import torchvision.transforms as T
+import uuid
 from attrdict import AttrDict
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
@@ -45,15 +46,31 @@ class MNISTModel(nn.Module):
 
 
 class Trainer:
-    def __init__(self, core_context, model, batch_size, metric_agg_rate_batches=1) -> None:
+    def __init__(
+        self,
+        core_context,
+        model: nn.Module,
+        worker_train_batch_size: int,
+        worker_val_batch_size: int,
+        metric_agg_rate_batches: int,
+        epochs: int,
+        val_freq: int,
+    ) -> None:
         self.core_context = core_context
         self.model = model
-        self.batch_size = batch_size
+        self.worker_train_batch_size = worker_train_batch_size
+        self.worker_val_batch_size = worker_val_batch_size
         self.metric_agg_rate_batches = metric_agg_rate_batches
+        self.metric_agg_rate_batches = metric_agg_rate_batches
+        self.epochs = epochs
+        self.val_freq = val_freq
 
         self.rank = core_context.distributed.rank
-        self.is_distributed = core_context.distributed.size > 1
+        self.local_rank = core_context.distributed.local_rank
+        self.size = core_context.distributed.size
+        self.is_distributed = self.size > 1
         self.is_chief = self.rank == 0
+        self.is_local_chief = self.local_rank == 0
         self.device = f"cuda:{self.rank}"
         self.model.to(self.device)
         if self.is_distributed:
@@ -79,7 +96,7 @@ class Trainer:
                 T.Normalize((0.1307,), (0.3081,)),
             ]
         )
-
+        worker_batch_size = self.worker_train_batch_size if train else self.worker_val_batch_size
         dataset = datasets.MNIST(
             root="shared_fs/data", train=train, download=False, transform=mnist_transform
         )
@@ -87,7 +104,7 @@ class Trainer:
             sampler = DistributedSampler(dataset, shuffle=True)
         else:
             sampler = RandomSampler(dataset) if train else SequentialSampler(dataset)
-        loader = DataLoader(dataset, batch_size=self.batch_size, sampler=sampler, drop_last=True)
+        loader = DataLoader(dataset, batch_size=worker_batch_size, sampler=sampler, drop_last=True)
         return loader
 
     def train_one_epoch(self) -> None:
@@ -95,6 +112,7 @@ class Trainer:
         for batch_idx, batch in enumerate(self.train_loader):
             self.trained_batches += 1
             images, labels = batch
+            print(f"{self.rank} BATCH_SIZE {images.shape[0]}")
             images, labels = images.to(self.device), labels.to(self.device)
             self.optimizer.zero_grad()
             outputs = self.model(images)
@@ -103,7 +121,6 @@ class Trainer:
             self.optimizer.step()
             if (self.trained_batches + 1) % self.metric_agg_rate_batches == 0:
                 computed_metrics = self.compute_metrics("train_")
-                computed_metrics["test_list"] = list(range(10))
                 if self.is_chief:
                     core_context.train.report_training_metrics(
                         steps_completed=self.trained_batches, metrics=computed_metrics
@@ -117,6 +134,7 @@ class Trainer:
         with torch.no_grad():
             for batch_idx, batch in enumerate(self.val_loader):
                 images, labels = batch
+                print(f"{self.rank} BATCH_SIZE {images.shape[0]}")
                 images, labels = images.to(self.device), labels.to(self.device)
                 outputs = self.model(images)
                 self.get_loss_and_update_metrics(outputs, labels)
@@ -131,12 +149,12 @@ class Trainer:
         if self.core_context.preempt.should_preempt():
             return
 
-    def train(self, epochs=1, val_freq=1) -> None:
-        for epoch_idx in range(epochs):
+    def train(self) -> None:
+        for epoch_idx in range(self.epochs):
             if self.is_chief:
                 print(80 * "*", f"Training during epoch {epoch_idx}", 80 * "*", sep="\n")
             self.train_one_epoch()
-            if (epoch_idx + 1) % val_freq == 0:
+            if (epoch_idx + 1) % self.val_freq == 0:
                 if self.is_chief:
                     print(80 * "*", f"Validating during epoch {epoch_idx}", 80 * "*", sep="\n")
                 self.validate()
@@ -160,11 +178,34 @@ class Trainer:
             met.reset()
         self.loss_metric.reset()
 
+    def checkpoint(self) -> None:
+        storage_manager = self.core_context.checkpoint._storage_manager
+        storage_id = str(uuid.uuid4())
+        if self.is_chief:
+            with storage_manager.store_path(storage_id) as path:
+                # Broadcast checkpoint path to all ranks.
+                self.core_context.distributed.broadcast((storage_id, path))
+                torch.save(self.model.state_dict(), path)
+                # Gather resources across nodes.
+                all_resources = self.core_context.distributed.gather(
+                    storage_manager._list_directory(path)
+                )
+            resources = {k: v for d in all_resources for k, v in d.items()}
+
+            self.core_context.checkpoint._report_checkpoint(storage_id, resources)
+        else:
+            storage_id, path = self.core_context.distributed.broadcast(None)
+            torch.save(self.model.state_dict(), path)
+            # Gather resources across nodes.
+            _ = self.core_context.distributed.gather(storage_manager._list_directory(path))
+            if self.is_local_chief:
+                storage_manager.post_store_path(storage_id, path)
+
 
 def main(core_context, hparams: AttrDict) -> None:
     model = MNISTModel(**hparams.model)
-    trainer = Trainer(core_context, model, batch_size=128, metric_agg_rate_batches=10)
-    trainer.train(epochs=1, val_freq=1)
+    trainer = Trainer(core_context, model, **hparams.trainer)
+    trainer.train()
 
 
 if __name__ == "__main__":
