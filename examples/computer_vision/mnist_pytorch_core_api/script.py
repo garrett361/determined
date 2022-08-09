4cc1d54257ccec611703394a7d51abea365a15ed
Garrett Goon <garrett.goon@hpe.com>
2022-08-09 11:16:34 -0400

    refactored into script with dict config

---
+++ examples/computer_vision/mnist_pytorch_core_api/script.py
@@ -0,0 +1,43 @@
+from determined.experimental import client
+
+MASTER = "http://104.196.135.13:8080"
+USER = "determined"
+PASSWORD = ""
+
+client.login(master=MASTER, user=USER, password=PASSWORD)
+
+model_hparams = {
+    "n_filters1": 32,
+    "n_filters2": 32,
+    "dropout1": 0.25,
+    "dropout2": 0.5,
+}
+
+optimizer_hparams = {"lr": 0.001}
+
+trainer_hparams = {
+    "worker_train_batch_size": 1024,
+    "worker_val_batch_size": 2048,
+    "train_metric_agg_rate": 8,
+    "epochs": 1,
+    "val_freq": 1,
+}
+
+config = {
+    "entrypoint": "python -m determined.launch.torch_distributed -- python3 main.py",
+    "name": "mnist_pytorch_core_api",
+    "workspace": "Core API",
+    "project": "MNIST",
+    "max_restarts": 0,
+    "reproducibility": {"experiment_seed": 42},
+    "resources": {"slots_per_trial": 1},
+    "searcher": {"name": "single", "max_length": trainer_hparams["epochs"], "metric": "val_loss"},
+    "environment": {"environment_variables": ["OMP_NUM_THREADS=1"]},
+    "hyperparameters": {
+        "model": model_hparams,
+        "optimizer": optimizer_hparams,
+        "trainer": trainer_hparams,
+    },
+}
+
+client.create_experiment(config=config, model_dir=".")
