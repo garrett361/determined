2a7c9ba07a0389cf7168d5f59c29f956cec1b43d
Garrett Goon <garrett.goon@hpe.com>
2022-08-10 14:20:56 -0400

    added cpu device case

--- examples/computer_vision/mnist_pytorch_core_api/trainer.py
+++ examples/computer_vision/mnist_pytorch_core_api/trainer.py
@@ -38,7 +38,7 @@ class Trainer:
         self.is_distributed = self.size > 1
         self.is_chief = self.rank == 0
         self.is_local_chief = self.local_rank == 0
-        self.device = f"cuda:{self.rank}"
+        self.device = "cpu" if not self.is_distributed else f"cuda:{self.rank}"
         self.model.to(self.device)
 
         if info.latest_checkpoint is None:
