82d9acb03da14e76aa2d4ea3155236f5a535eb30
Garrett Goon <garrett.goon@hpe.com>
2023-01-09 15:06:45 -0500

    minor changes

--- ds_autotuning_prototype/dsat/dsat_experiment_wrapper.py
+++ ds_autotuning_prototype/dsat/dsat_experiment_wrapper.py
@@ -17,6 +17,7 @@ def main(core_context: det.core.Context) -> None:
     # I don't think the below copy is needed, but just being safe.
     profiling_config_dict = copy.deepcopy(original_config_dict)
     for op in core_context.searcher.operations():
+        # Submit the actual Trial of interest and wait on its results.
         profiling_config_dict["name"] += " (individual autotuning trial)"
         profiling_config_dict["searcher"] = {
             "name": "single",
@@ -32,12 +33,17 @@ def main(core_context: det.core.Context) -> None:
         ] += "; python3 -m determined.launch.torch_distributed python3 -m dsat.checkpoint_profiling_results"
         exp = client.create_experiment(config=profiling_config_dict, model_dir=".")
         exp_exit_status = exp.wait()  # TODO: Error handling.
+
+        # Get the results from the checkpoint (super hacky).
         trial = exp.get_trials()[0]
         ckpt = trial.select_checkpoint(latest=True)
         ckpt.download(path=".")
         profiler_results = utils.DSProfilerResults(path=constants.OUTPUT_FILE_PATH)
         results_dict = profiler_results.get_results_dict()
-        core_context.train.report_validation_metrics(steps_completed=0, metric=results_dict)
+        print("RESULTS_DICT", results_dict)
+
+        # Report the results to the searcher and Web UI.
+        core_context.train.report_validation_metrics(steps_completed=0, metrics=results_dict)
         op.report_completed(results_dict[profiling_config_dict["searcher"]["metric"]])
 
 
