726eba103022616e75f136bf4456b56a7146fff0
Garrett Goon <garrett.goon@hpe.com>
2023-02-08 16:12:58 -0500

    minor changes

--- examples/custom_gc_policy/main.py
+++ examples/custom_gc_policy/main.py
@@ -93,13 +93,20 @@ def main() -> None:
             smaller_is_better=False,
             sort_by="arbitrary_metric_name",
         )
-        logging.info(f"{[c.state for c in reloaded_checkpoints]}")
+        reloaded_checkpoint_states = [c.state for c in reloaded_checkpoints]
+        assert all(
+            [s.DELETED for s in reloaded_checkpoint_states]
+        ), "Some checkpoints were not deleted!"
+        logging.info(f"All checkpoints successfully deleted.")
 
         # Wait a bit and try to delete them again.
         logging.info("Attempting to delete again.")
         time.sleep(5)
-        for ckpt in reloaded_checkpoints:
-            ckpt.delete()
+        try:
+            for ckpt in reloaded_checkpoints:
+                ckpt.delete()
+        except Exception as e:
+            logging.info(f"Repeated deletion attempt raised error {e}")
 
 
 if __name__ == "__main__":
