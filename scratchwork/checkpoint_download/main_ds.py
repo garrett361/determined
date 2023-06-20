7310a1ba3c558ebf4486eed2fd36718dfe0fc29d
Garrett Goon <garrett.goon@hpe.com>
2023-06-20 16:51:18 -0400

    adding ds checkpoint download tests

--- scratchwork/checkpoint_download/main_ds.py
+++ scratchwork/checkpoint_download/main_ds.py
@@ -15,7 +15,7 @@ def main(core_context) -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format=det.LOG_FORMAT)
     try:
-        distributed = det.core.DistributedContext.from_torch_distributed()
+        distributed = det.core.DistributedContext.from_deepspeed()
     except KeyError:
         distributed = None
     with det.core.init(distributed=distributed) as core_context:
