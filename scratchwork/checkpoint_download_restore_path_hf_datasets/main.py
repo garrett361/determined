efba8b7e5a4607f3a990488c3d22ea9b7467c70e
Garrett Goon <garrett.goon@hpe.com>
2023-07-12 13:34:43 -0400

    more  scratchwork

--- scratchwork/checkpoint_download_restore_path_hf_datasets/main.py
+++ scratchwork/checkpoint_download_restore_path_hf_datasets/main.py
@@ -4,23 +4,14 @@ import os
 from datasets import load_from_disk
 
 import determined as det
-from determined.experimental.client import get_checkpoint
 
 
 def main(core_context) -> None:
     logging.info(f"ENVVARS: {os.environ}")
     ckpt_uuid = "f1937b0d-f56b-4653-99e5-06b4e11425b6"
-    try:
-        logging.info("Attemping download with get_checkpoint")
-        dataset_checkpoint = get_checkpoint(ckpt_uuid)
-        dataset_checkpoint_path = dataset_checkpoint.download()
-        logging.info(str(dataset_checkpoint_path))
+    with core_context.checkpoint.restore_path(ckpt_uuid) as dataset_checkpoint_path:
         dset = load_from_disk(dataset_checkpoint_path)
-    except Exception as e:
-        logging.info(f"Hit exception {e}, attempting to download with restore_path")
-        with core_context.checkpoint.restore_path(ckpt_uuid) as path:
-            dset = load_from_disk(dataset_checkpoint_path)
-            logging.info(f"Path: {path}")
+        logging.info(f"Path: {dataset_checkpoint_path}, Dataset: {dset}")
 
 
 if __name__ == "__main__":
